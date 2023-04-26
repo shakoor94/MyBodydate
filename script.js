63f4e8bd0d1655549ca352478c937d4cb1310205
Sididaoudi <sididaoudi24@gmail.com>
2023-04-26 09:09:47 +0200

    pages amoureux et pro
    new pages

--- script.js
+++ script.js
@@ -16,33 +16,53 @@ themeToggle.addEventListener('click', function () {
 
 /* Animation img Index page */
 
-const imgTalk = document.querySelector('.imgTalk');
+// const imgTalk = document.querySelector('.imgTalk');
 
-const observer = new IntersectionObserver(entries => {
+// const observer = new IntersectionObserver(entries => {
 
-    if (entries[0].isIntersecting) {
+//     if (entries[0].isIntersecting) {
 
-        imgTalk.classList.add('appear');
+//         imgTalk.classList.add('appear');
 
-        observer.unobserve(imgTalk);
-    }
+//         observer.unobserve(imgTalk);
+//     }
+// });
+
+// observer.observe(imgTalk);
+
+
+
+
+// let rencontre = document.querySelector(".fa-sharp");
+// let sous_menu = document.querySelector(".sous-menu")
+
+
+// rencontre.addEventListener("click", () => {
+//     if (getComputedStyle(sous_menu).display != "block") {
+//         sous_menu.style.display = "block"; {
+//         }
+//     }
+//     else {
+//         sous_menu.style.display = "none";
+//     }
+// })
+
+/* Filtres */
+const btnFilter = document.querySelector('.btnFilter');
+
+btnFilter.addEventListener('click', () => {
+    console.log('Bouton Valider cliqué');
+    // Récupérer les valeurs sélectionnées pour chaque filtre
+    const selectedGender = document.querySelector('#gender').value;
+    const selectedAge = Array.from(document.querySelectorAll('input[name="age"]:checked'))
+        .map(checkbox => checkbox.value)
+        .join(',');
+    const selectedRelation = document.querySelector('#relation').value;
+
+    // Construire l'URL de redirection avec les critères sélectionnés
+    const url = selectedGender === "homme" ? `meetMen.html?gender=${selectedGender}&age=${selectedAge}&relation=${selectedRelation}` : `meetWomen.html?gender=${selectedGender}&age=${selectedAge}&relation=${selectedRelation}`;
+
+
+    // Rediriger l'utilisateur vers la page meet.html avec les critères sélectionnés comme paramètres d'URL
+    window.location.href = url;
 });
-
-observer.observe(imgTalk);
-
-
-
-
-let rencontre = document.querySelector(".fa-sharp");
-let sous_menu = document.querySelector(".sous-menu")
-
-
-rencontre.addEventListener("click", () => {
-    if (getComputedStyle(sous_menu).display != "block") {
-        sous_menu.style.display = "block"; {
-        }
-    }
-    else {
-        sous_menu.style.display = "none";
-    }
-})
